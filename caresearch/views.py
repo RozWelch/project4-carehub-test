08d7e26477d1d7faaba2b06e64e32a7d0483fb01
RozWelch <rozwelch@gmail.com>
2023-01-01 15:39:34 +0000

    Add view.py and templates

--- caresearch/views.py
+++ caresearch/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Careprovider, Careseeker
 
-# Create your views here.
+class CareproviderList(generic.ListView):
+    model = Careprovider
+    queryset = Careprovider.objects.order_by('address_line3')
+    template_name = 'index.html'
+    paginate_by = 6
